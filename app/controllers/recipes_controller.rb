27328dcf76e19ec08b2bb0eeaa7797dd44a72cdd
Eric Butler <ericbutler555@gmail.com>
2017-03-18 20:19:00 +0000

    Refactored some duplicated code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,13 @@
 class RecipesController < ApplicationController
+
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
   
   def index
     @recipes = Recipe.all
   end
   
   def show
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
   end
   
   def new
@@ -26,11 +28,11 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       flash[:success] = "Recipe was updated successfully"
       redirect_to recipe_path(@recipe)
@@ -40,7 +42,7 @@ class RecipesController < ApplicationController
   end
   
   def destroy
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
     if @recipe.destroy
       flash[:success] = "Recipe deleted successfully"
       redirect_to recipes_path
@@ -51,6 +53,10 @@ class RecipesController < ApplicationController
   end
   
   private
+  
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
   
     def recipe_params
       params.require(:recipe).permit(:name, :description, :chef_id)
